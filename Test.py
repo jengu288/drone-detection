27d4d277641951510453f7677592e86bf4d0c3a3
jengu <jen.jennifer.gu@gmail.com>
2019-10-24 18:58:25 -0700

    adding better test image

--- Test.py
+++ Test.py
@@ -1,1 +1,31 @@
-print("this works now")
+import cv2
+
+# load the input image and show its dimensions, keeping in mind that
+# images are represented as a multi-dimensional NumPy array with
+# shape no. rows (height) x no. columns (width) x no. channels (depth).
+# Depth is the number of channels  — in our case this is three since
+# we’re working with 3 color channels: Blue, Green, and Red
+image = cv2.imread("download.jpeg")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+
+# display the image to our screen -- need to click the window
+# open by OpenCV and press a key on our keyb oard to continue execution
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# access the RGB pixel located at x=50, y=100, keeping in mind that
+# OpenCV stores images in BGR order rather than RGB
+(B, G, R) = image[100, 50]
+print("R={}, G={}, B={}".format(R, G, B))
+
+# extract a 100x100 pixel square ROI (Region of Interest) from the
+# input image starting at x=320,y=60 at ending at x=420,y=160
+roi = image[60:160, 320:420]
+cv2.imshow("ROI", roi)
+cv2.waitKey(0)
+
+# resize the image to 200x200px, ignoring aspect ratio
+resized = cv2.resize(image, (200, 200))
+cv2.imshow("Fixed Resizing", resized)
+cv2.waitKey(0)
